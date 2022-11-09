df8bbc259dbe8ca97561c42f1348d007becc1c3a
Thetechnology <sandhya.rani.ydv@gmail.com>
2022-11-09 18:46:30 +0530

    Initial commit

---
+++ src/main/java/com/example/chatroomapp/models/Message.java
@@ -0,0 +1,19 @@
+package com.example.chatroomapp.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+
+    private String name;
+
+    private String content;
+
+}
---
+++ src/test/java/com/example/chatroomapp/ChatroomappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatroomapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatroomappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
